57fc0e2186243f3d23dbae94a436eb952bb446c6
tigo <tigo>
2015-01-24 03:01:57 +0000

    add test SortTest

---
+++ test/SortTest.java
@@ -0,0 +1,44 @@
+import org.junit.*;
+import play.test.*;
+import play.mvc.*;
+import play.mvc.Http.*;
+import models.*;
+import java.util.*;
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+import play.vfs.VirtualFile;
+
+public class SortTest extends FunctionalTest {
+    
+    @Test
+    public void testThatSortWorks() {
+        String jsonUnsorted = "";
+        String jsonSorted = "";
+        try {
+            jsonUnsorted = FileUtils.readFileToString(VirtualFile.fromRelativePath("/test/customer-ordering.json").getRealFile(), "utf-8");
+            jsonSorted = FileUtils.readFileToString(VirtualFile.fromRelativePath("/test/sorted.json").getRealFile(), "utf-8");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        Map<String, String> paramMap = new HashMap<String, String>();
+        Map<String, File> fileMap = new HashMap<String, File>();
+        paramMap.put("json", jsonUnsorted);
+        //fileMap.put("xmlFile", new File("test/item.xml);
+        Response response = POST("/addJson", paramMap, fileMap);
+        assertIsOk(response);
+        System.out.println(response);
+        assertEquals(jsonSorted, getContent(response));
+
+    }
+
+    public static String getContent(Response response) {
+        byte[] data = response.out.toByteArray();
+        try {
+            return new String(data, response.encoding);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+}
