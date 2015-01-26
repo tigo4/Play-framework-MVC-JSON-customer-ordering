ca131269c7f3991571199181da961314fca83640
tigo <tigo>
2015-01-26 03:27:55 +0000

    test performance

--- benchmark/test-performance-on-branch_initial-solution.sh
+++ benchmark/test-performance-on-branch_initial-solution.sh
@@ -4,7 +4,11 @@ if [ ! -d target ]; then
     mkdir target;
 fi
 
+jmeter -n -t index.jmx -j jmeter.log -p jmeter.properties | tee /dev/null
+
 jmeter -n -t sortJson.jmx -j jmeter.log -p jmeter.properties | tee target/sortJson.log
+
+jmeter -n -t index.jmx -j jmeter.log -p jmeter.properties | tee /dev/null
 
 jmeter -n -t sortJsonBlock.jmx -j jmeter.log -p jmeter.properties | tee target/sortJsonBlock.log
 
